37c170866b4ced55d4d5e8028aeb3f01a1452d83
William Broussard <wbroussard@me.com>
2020-09-08 18:36:23 -0500

    Updates

---
+++ precache-manifest.61ecd6105e9bc04ba8bc50e59471f10c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e147b97d1a623c9cf2074b8203c906",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "851b13d3544ed2cafd99",
+    "url": "/React_Portfolio/static/css/2.207c7e13.chunk.css"
+  },
+  {
+    "revision": "11d902593514f87479f0",
+    "url": "/React_Portfolio/static/css/main.b04f6d08.chunk.css"
+  },
+  {
+    "revision": "851b13d3544ed2cafd99",
+    "url": "/React_Portfolio/static/js/2.466339a0.chunk.js"
+  },
+  {
+    "revision": "11d902593514f87479f0",
+    "url": "/React_Portfolio/static/js/main.27679794.chunk.js"
+  },
+  {
+    "revision": "62b34e686dbbee9cedcc",
+    "url": "/React_Portfolio/static/js/runtime~main.47646755.js"
+  },
+  {
+    "revision": "fb92f7da2954bc95bf2ed77d893e2c62",
+    "url": "/React_Portfolio/static/media/Day_Planner.fb92f7da.png"
+  },
+  {
+    "revision": "a58364d03800a1c02ae8e222a8feb63d",
+    "url": "/React_Portfolio/static/media/Employee_Summary.a58364d0.png"
+  },
+  {
+    "revision": "9356c69491544e43c54d8db824f9f32e",
+    "url": "/React_Portfolio/static/media/Foodies.9356c694.png"
+  },
+  {
+    "revision": "9295eb7fb8c62ca1bec370663754346f",
+    "url": "/React_Portfolio/static/media/Me.9295eb7f.png"
+  },
+  {
+    "revision": "f351021d726c354e39e085fcc5d172a7",
+    "url": "/React_Portfolio/static/media/Note_Taker.f351021d.png"
+  },
+  {
+    "revision": "5e30abd8e72edc89271271d442d5adc8",
+    "url": "/React_Portfolio/static/media/Weather_Dashboard.5e30abd8.png"
+  },
+  {
+    "revision": "d7b66acc642541f1a16141a80642b85d",
+    "url": "/React_Portfolio/static/media/Web_Organizer.d7b66acc.png"
+  },
+  {
+    "revision": "6973a2b5ef52075a367b74a3e1413a48",
+    "url": "/React_Portfolio/static/media/William_Broussard_Resume.6973a2b5.pdf"
+  }
+]);
